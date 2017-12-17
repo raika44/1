30dc1526ed85bc0c04dd67b38edcc8afea86ade9
iamwijaya <31705906+iamwijaya@users.noreply.github.com>
2017-12-17 21:46:09 +0700

    Add files via upload

---
+++ 11.py
@@ -0,0 +1,8 @@
+GNU nano 2.5.3 File: 1.py
+# -*- coding: utf-8 -*-
+import LINETCR from LINETCR.lib.curve.ttypes import * from datetime import 
+datetime import time,random,sys,json,codecs,threading,glob,re
+#kk = LINETCR.LINE() kk.login(qr=True) kk.loginResult()
+cl = LINETCR.LINE() #A) 
+cl.login(token="En6HQUwqP2weqBgHbC92.WCCRkMNgQXINSOQNF/5yiG.4uREMlH/k7c$
+ki = kk = kc = ks = ka = kb = ko = ke = ku = cl
